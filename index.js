ba30c6e30c6aa45f87cbdd3dc3ea46c2b714a54a
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-02-01 17:24:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,11 +23,8 @@ function dec(n) {
 }
 
 var b = 10; 
-function makeInt(n, b) {
-  var a = parseInt('n', b);
-  
-  if (isNaN(a)) { return NaN }
-  return a * 100;
+function makeInt(n) {
+  return parseInt(n, 10)
 }
 
 function preserveDecimal(n) {
